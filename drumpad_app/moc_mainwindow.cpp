64307212169073aa3bf8a69357e90e709419d789
JamessCz <jamessczvideos@gmail.com>
2021-04-13 11:26:54 +0200

    preupload changes

---
+++ drumpad_app/moc_mainwindow.cpp
@@ -0,0 +1,332 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.6)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.12.6. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[54];
+    char stringdata0[981];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 13), // "playMetronome"
+QT_MOC_LITERAL(2, 25, 0), // ""
+QT_MOC_LITERAL(3, 26, 9), // "playHihat"
+QT_MOC_LITERAL(4, 36, 15), // "playOpenedHihat"
+QT_MOC_LITERAL(5, 52, 9), // "playSnare"
+QT_MOC_LITERAL(6, 62, 7), // "playRim"
+QT_MOC_LITERAL(7, 70, 8), // "playKick"
+QT_MOC_LITERAL(8, 79, 10), // "playTomOne"
+QT_MOC_LITERAL(9, 90, 10), // "playTomTwo"
+QT_MOC_LITERAL(10, 101, 12), // "playFloorTom"
+QT_MOC_LITERAL(11, 114, 9), // "playCrash"
+QT_MOC_LITERAL(12, 124, 10), // "playSplash"
+QT_MOC_LITERAL(13, 135, 8), // "playRide"
+QT_MOC_LITERAL(14, 144, 23), // "on_spinBox_valueChanged"
+QT_MOC_LITERAL(15, 168, 4), // "arg1"
+QT_MOC_LITERAL(16, 173, 29), // "on_metronome_checkBox_toggled"
+QT_MOC_LITERAL(17, 203, 7), // "checked"
+QT_MOC_LITERAL(18, 211, 20), // "on_dial_valueChanged"
+QT_MOC_LITERAL(19, 232, 5), // "value"
+QT_MOC_LITERAL(20, 238, 20), // "on_buttonOne_clicked"
+QT_MOC_LITERAL(21, 259, 20), // "on_buttonTwo_clicked"
+QT_MOC_LITERAL(22, 280, 22), // "on_buttonThree_clicked"
+QT_MOC_LITERAL(23, 303, 21), // "on_buttonFour_clicked"
+QT_MOC_LITERAL(24, 325, 21), // "on_buttonFive_clicked"
+QT_MOC_LITERAL(25, 347, 20), // "on_buttonSix_clicked"
+QT_MOC_LITERAL(26, 368, 22), // "on_buttonSeven_clicked"
+QT_MOC_LITERAL(27, 391, 22), // "on_buttonEight_clicked"
+QT_MOC_LITERAL(28, 414, 21), // "on_buttonNine_clicked"
+QT_MOC_LITERAL(29, 436, 23), // "on_hihat_toggle_toggled"
+QT_MOC_LITERAL(30, 460, 26), // "on_snareRim_toggle_toggled"
+QT_MOC_LITERAL(31, 487, 13), // "keyPressEvent"
+QT_MOC_LITERAL(32, 501, 10), // "QKeyEvent*"
+QT_MOC_LITERAL(33, 512, 1), // "e"
+QT_MOC_LITERAL(34, 514, 15), // "keyReleaseEvent"
+QT_MOC_LITERAL(35, 530, 26), // "on_volumeDial_valueChanged"
+QT_MOC_LITERAL(36, 557, 26), // "on_record_checkBox_toggled"
+QT_MOC_LITERAL(37, 584, 20), // "on_repeatOne_toggled"
+QT_MOC_LITERAL(38, 605, 20), // "on_repeatTwo_toggled"
+QT_MOC_LITERAL(39, 626, 22), // "on_repeatThree_toggled"
+QT_MOC_LITERAL(40, 649, 21), // "on_repeatFour_toggled"
+QT_MOC_LITERAL(41, 671, 21), // "on_repeatFive_toggled"
+QT_MOC_LITERAL(42, 693, 20), // "on_repeatSix_toggled"
+QT_MOC_LITERAL(43, 714, 22), // "on_repeatSeven_toggled"
+QT_MOC_LITERAL(44, 737, 22), // "on_repeatEight_toggled"
+QT_MOC_LITERAL(45, 760, 21), // "on_repeatNine_toggled"
+QT_MOC_LITERAL(46, 782, 32), // "on_enableRepeat_checkBox_toggled"
+QT_MOC_LITERAL(47, 815, 29), // "on_repeatAll_checkBox_toggled"
+QT_MOC_LITERAL(48, 845, 17), // "on_tapBPM_clicked"
+QT_MOC_LITERAL(49, 863, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(50, 885, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(51, 909, 24), // "on_easterEgg_btn_clicked"
+QT_MOC_LITERAL(52, 934, 23), // "on_svejdaButton_clicked"
+QT_MOC_LITERAL(53, 958, 22) // "on_nemecButton_clicked"
+
+    },
+    "MainWindow\0playMetronome\0\0playHihat\0"
+    "playOpenedHihat\0playSnare\0playRim\0"
+    "playKick\0playTomOne\0playTomTwo\0"
+    "playFloorTom\0playCrash\0playSplash\0"
+    "playRide\0on_spinBox_valueChanged\0arg1\0"
+    "on_metronome_checkBox_toggled\0checked\0"
+    "on_dial_valueChanged\0value\0"
+    "on_buttonOne_clicked\0on_buttonTwo_clicked\0"
+    "on_buttonThree_clicked\0on_buttonFour_clicked\0"
+    "on_buttonFive_clicked\0on_buttonSix_clicked\0"
+    "on_buttonSeven_clicked\0on_buttonEight_clicked\0"
+    "on_buttonNine_clicked\0on_hihat_toggle_toggled\0"
+    "on_snareRim_toggle_toggled\0keyPressEvent\0"
+    "QKeyEvent*\0e\0keyReleaseEvent\0"
+    "on_volumeDial_valueChanged\0"
+    "on_record_checkBox_toggled\0"
+    "on_repeatOne_toggled\0on_repeatTwo_toggled\0"
+    "on_repeatThree_toggled\0on_repeatFour_toggled\0"
+    "on_repeatFive_toggled\0on_repeatSix_toggled\0"
+    "on_repeatSeven_toggled\0on_repeatEight_toggled\0"
+    "on_repeatNine_toggled\0"
+    "on_enableRepeat_checkBox_toggled\0"
+    "on_repeatAll_checkBox_toggled\0"
+    "on_tapBPM_clicked\0on_pushButton_clicked\0"
+    "on_pushButton_2_clicked\0"
+    "on_easterEgg_btn_clicked\0"
+    "on_svejdaButton_clicked\0on_nemecButton_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      47,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  249,    2, 0x08 /* Private */,
+       3,    0,  250,    2, 0x08 /* Private */,
+       4,    0,  251,    2, 0x08 /* Private */,
+       5,    0,  252,    2, 0x08 /* Private */,
+       6,    0,  253,    2, 0x08 /* Private */,
+       7,    0,  254,    2, 0x08 /* Private */,
+       8,    0,  255,    2, 0x08 /* Private */,
+       9,    0,  256,    2, 0x08 /* Private */,
+      10,    0,  257,    2, 0x08 /* Private */,
+      11,    0,  258,    2, 0x08 /* Private */,
+      12,    0,  259,    2, 0x08 /* Private */,
+      13,    0,  260,    2, 0x08 /* Private */,
+      14,    1,  261,    2, 0x08 /* Private */,
+      16,    1,  264,    2, 0x08 /* Private */,
+      18,    1,  267,    2, 0x08 /* Private */,
+      20,    0,  270,    2, 0x08 /* Private */,
+      21,    0,  271,    2, 0x08 /* Private */,
+      22,    0,  272,    2, 0x08 /* Private */,
+      23,    0,  273,    2, 0x08 /* Private */,
+      24,    0,  274,    2, 0x08 /* Private */,
+      25,    0,  275,    2, 0x08 /* Private */,
+      26,    0,  276,    2, 0x08 /* Private */,
+      27,    0,  277,    2, 0x08 /* Private */,
+      28,    0,  278,    2, 0x08 /* Private */,
+      29,    1,  279,    2, 0x08 /* Private */,
+      30,    1,  282,    2, 0x08 /* Private */,
+      31,    1,  285,    2, 0x08 /* Private */,
+      34,    1,  288,    2, 0x08 /* Private */,
+      35,    1,  291,    2, 0x08 /* Private */,
+      36,    1,  294,    2, 0x08 /* Private */,
+      37,    1,  297,    2, 0x08 /* Private */,
+      38,    1,  300,    2, 0x08 /* Private */,
+      39,    1,  303,    2, 0x08 /* Private */,
+      40,    1,  306,    2, 0x08 /* Private */,
+      41,    1,  309,    2, 0x08 /* Private */,
+      42,    1,  312,    2, 0x08 /* Private */,
+      43,    1,  315,    2, 0x08 /* Private */,
+      44,    1,  318,    2, 0x08 /* Private */,
+      45,    1,  321,    2, 0x08 /* Private */,
+      46,    1,  324,    2, 0x08 /* Private */,
+      47,    1,  327,    2, 0x08 /* Private */,
+      48,    0,  330,    2, 0x08 /* Private */,
+      49,    0,  331,    2, 0x08 /* Private */,
+      50,    0,  332,    2, 0x08 /* Private */,
+      51,    0,  333,    2, 0x08 /* Private */,
+      52,    0,  334,    2, 0x08 /* Private */,
+      53,    0,  335,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,   15,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Int,   19,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, 0x80000000 | 32,   33,
+    QMetaType::Void, 0x80000000 | 32,   33,
+    QMetaType::Void, QMetaType::Int,   19,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void, QMetaType::Bool,   17,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->playMetronome(); break;
+        case 1: _t->playHihat(); break;
+        case 2: _t->playOpenedHihat(); break;
+        case 3: _t->playSnare(); break;
+        case 4: _t->playRim(); break;
+        case 5: _t->playKick(); break;
+        case 6: _t->playTomOne(); break;
+        case 7: _t->playTomTwo(); break;
+        case 8: _t->playFloorTom(); break;
+        case 9: _t->playCrash(); break;
+        case 10: _t->playSplash(); break;
+        case 11: _t->playRide(); break;
+        case 12: _t->on_spinBox_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->on_metronome_checkBox_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 14: _t->on_dial_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 15: _t->on_buttonOne_clicked(); break;
+        case 16: _t->on_buttonTwo_clicked(); break;
+        case 17: _t->on_buttonThree_clicked(); break;
+        case 18: _t->on_buttonFour_clicked(); break;
+        case 19: _t->on_buttonFive_clicked(); break;
+        case 20: _t->on_buttonSix_clicked(); break;
+        case 21: _t->on_buttonSeven_clicked(); break;
+        case 22: _t->on_buttonEight_clicked(); break;
+        case 23: _t->on_buttonNine_clicked(); break;
+        case 24: _t->on_hihat_toggle_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 25: _t->on_snareRim_toggle_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 26: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 27: _t->keyReleaseEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 28: _t->on_volumeDial_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 29: _t->on_record_checkBox_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 30: _t->on_repeatOne_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 31: _t->on_repeatTwo_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 32: _t->on_repeatThree_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 33: _t->on_repeatFour_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 34: _t->on_repeatFive_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 35: _t->on_repeatSix_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 36: _t->on_repeatSeven_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 37: _t->on_repeatEight_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 38: _t->on_repeatNine_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 39: _t->on_enableRepeat_checkBox_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 40: _t->on_repeatAll_checkBox_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 41: _t->on_tapBPM_clicked(); break;
+        case 42: _t->on_pushButton_clicked(); break;
+        case 43: _t->on_pushButton_2_clicked(); break;
+        case 44: _t->on_easterEgg_btn_clicked(); break;
+        case 45: _t->on_svejdaButton_clicked(); break;
+        case 46: _t->on_nemecButton_clicked(); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 47)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 47;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 47)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 47;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
